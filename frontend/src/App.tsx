093845ef4e55a18e313d26d58d995717724264ba
Matthew Davis <mdavis@insights.com>
2024-05-03 16:16:08 +0100

    Started to get tailwind working

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navigation from './components/Navigation.tsx';
+// import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <Navigation />
+      <h1>Hello</h1>
+      <div className="bg-blue-500 text-white p-4">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -26,7 +18,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      Click on the Vite and React logos to learn more
       </p>
     </>
   )
---
+++ frontend/src/components/Navigation.tsx
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom';
+
+export default function Navigation() {
+
+  return (
+    <div className="fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto bg-gray-900 lg:translate-x-0 lg:static lg:inset-0">
+      <div className="flex items-center justify-center mt-7">
+        <div className="flex items-center">
+          <Link to="/">
+            <span className="mx-2 text-xl font-semibold text-white">Python FM Playground</span>
+          </Link>
+        </div>
+      </div>
+      <nav className="mt-10">
+        <Link to="/models"
+              className="flex items-center px-6 py-2 mt-4 text-gray-400 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100">
+          <span className="mx-3">Foundation Models</span>
+        </Link>
+        <Link to="/chat"
+              className="flex items-center px-6 py-2 mt-4 text-gray-400 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100">
+          <span className="mx-3">Chat Playground</span>
+        </Link>
+        <Link to="/text"
+              className="flex items-center px-6 py-2 mt-4 text-gray-400 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100">
+          <span className="mx-3">Text Playground</span>
+        </Link>
+        <Link to="/image"
+              className="flex items-center px-6 py-2 mt-4 text-gray-400 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100">
+          <span className="mx-3">Image Playground</span>
+        </Link>
+      </nav>
+    </div>
+  );
+}
